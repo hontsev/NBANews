f13c84f7be3a59c9a9d463ac4cbea1c2a9d8958f
hontsev <hontsev@gmail.com>
2017-11-06 00:28:12 +0800

    数据抓取+数据库

---
+++ NBAMatchDataGetter/NBAMatchDataGetter/Getter.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
+using System.Data;
+
+namespace NBAMatchDataGetter
+{
+
+    public class Getter
+    {
+        private static void saveMatchInfo(string id)
+        {
+            string url = string.Format("http://api.sports.sina.com.cn/?p=nba&s=live&a=matchInfo&format=json&id={0}&dpc=1", id);
+            JsonSerializer serializer = new JsonSerializer();
+            string jsonstr = WebHelper.getData(url, Encoding.UTF8);
+            string cjsonstr= Regex.Unescape(jsonstr);
+            JObject o = JObject.Parse(cjsonstr);
+            string date = o["result"]["data"]["score"]["remain"]["date"].ToString();
+            string time= o["result"]["data"]["score"]["remain"]["time"].ToString();
+            
+            int quarter = int.Parse(o["result"]["data"]["score"]["remain"]["quarter"].ToString());
+            int minutes = int.Parse(o["result"]["data"]["score"]["remain"]["minutes"].ToString());
+            int seconds = int.Parse(o["result"]["data"]["score"]["remain"]["seconds"].ToString());
+            int period= int.Parse(o["result"]["data"]["score"]["remain"]["period"].ToString());
+            int hostid= int.Parse(o["result"]["data"]["score"]["host"]["team_id"].ToString());
+            int guestid = int.Parse(o["result"]["data"]["score"]["guest"]["team_id"].ToString());
+            string team1 = o["result"]["data"]["matchInfo"]["team1_name"].ToString();
+            int team1id = int.Parse(o["result"]["data"]["matchInfo"]["team1_id"].ToString());
+            string team2 =o["result"]["data"]["matchInfo"]["team2_name"].ToString();
+            int team2id = int.Parse(o["result"]["data"]["matchInfo"]["team2_id"].ToString());
+            var guestscore = o["result"]["data"]["score"]["guest"]["scores"].ToArray();
+            var hostscore = o["result"]["data"]["score"]["host"]["scores"].ToArray();
+
+            // sort guest and host 
+            string guestname = "";
+            string hostname = "";
+            if (guestid == team1id)
+            {
+                guestname = o["result"]["data"]["matchInfo"]["team1_name"].ToString();
+                hostname = o["result"]["data"]["matchInfo"]["team2_name"].ToString();
+            }
+            else
+            {
+                hostname = o["result"]["data"]["matchInfo"]["team1_name"].ToString();
+                guestname = o["result"]["data"]["matchInfo"]["team2_name"].ToString();
+            }
+            
+
+            // set score
+            int gscore = 0;
+            int hscore = 0;
+            foreach (var s in guestscore) gscore += int.Parse(s.ToString());
+            foreach (var s in hostscore) hscore += int.Parse(s.ToString());
+
+            // set date
+            string begintime = date + " " + time;
+            //DateTime dbegintime = Convert.ToDateTime(begintime);
+
+            if(!MySqlHelper.ExistData(string.Format("SELECT id FROM nbamatch.team WHERE id='{0}'", guestid)))
+            {
+                MySqlHelper.Execute(string.Format("INSERT INTO nbamatch.team(id,name) VALUES('{0}','{1}')", guestid, guestname));
+            }
+            if (!MySqlHelper.ExistData(string.Format("SELECT id FROM nbamatch.team WHERE id='{0}'", hostid)))
+            {
+                MySqlHelper.Execute(string.Format("INSERT INTO nbamatch.team(id,name) VALUES('{0}','{1}')", hostid, hostname));
+            }
+
+            if (!MySqlHelper.ExistData(string.Format("SELECT id FROM nbamatch.match WHERE begintime='{0}' and guestteam='{1}' and hostteam='{2}' ", begintime, guestid, hostid)))
+            {
+                MySqlHelper.Execute(string.Format("INSERT INTO nbamatch.match(id,beginTime,guestteam,hostteam,guestscore,hostscore) VALUES('{0}','{1}','{2}','{3}','{4}','{5}')", 
+                    id,begintime, guestid, hostid, gscore, hscore));
+            }
+
+
+            return;
+        }
+        /// <summary>  
+        /// 时间戳Timestamp转换成日期  
+        /// </summary>  
+        /// <param name="timeStamp"></param>  
+        /// <returns></returns>  
+        private static DateTime GetDateTime(string timeStamp)
+        {
+            DateTime dtStart = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1));
+            long lTime = long.Parse(timeStamp + "0000000");
+            TimeSpan toNow = new TimeSpan(lTime);
+            DateTime targetDt = dtStart.Add(toNow);
+            return dtStart.Add(toNow);
+        }
+
+        private static DateTime GetDateTime(long timeStamp)
+        {
+            return GetDateTime(timeStamp.ToString());
+        }
+
+        private static void saveEventInfo(string id)
+        {
+            string url = string.Format("http://api.sports.sina.com.cn/pbp/?format=json&source=show&mid={0}&pid=&eid=0&dpc=1", id);
+            JsonSerializer serializer = new JsonSerializer();
+            string jsonstr = WebHelper.getData(url, Encoding.UTF8);
+            string cjsonstr = Regex.Unescape(jsonstr);
+            JObject o = JObject.Parse(cjsonstr);
+
+            int num = int.Parse(o["result"]["data"]["last_eid"].ToString());
+            var oe = o["result"]["data"]["pbp_msgs"].ToArray();
+            for (int i = 0; i < oe.Length; i++)
+            {
+                JObject eo = JObject.Parse(oe[i].First().ToString());
+                int period = int.Parse(eo["period"].ToString());
+                string game_clock = eo["game_clock"].ToString();
+                int home_score =int.Parse( eo["home_score"].ToString());
+                int visitor_score = int.Parse(eo["visitor_score"].ToString());
+                string description = eo["description"].ToString();
+                int team_id = int.Parse(eo["team_id"].ToString());
+                string last_name = eo["last_name"].ToString();
+                long updated = long.Parse(eo["updated"].ToString());
+                DateTime updatetime = GetDateTime(updated);
+                int scored = int.Parse(eo["scored"].ToString());
+                int personid = int.Parse(eo["person_id"].ToString());
+                int event_num = int.Parse(eo["event_num"].ToString());
+
+                int type = 0;
+                if (!(personid==0) && !MySqlHelper.ExistData(string.Format("SELECT id FROM nbamatch.person WHERE name='{0}'", last_name)))
+                {
+                    MySqlHelper.Execute(string.Format("INSERT INTO nbamatch.person(id,name) VALUES('{0}','{1}')", personid, last_name));
+                }
+
+                if (!MySqlHelper.ExistData(string.Format("SELECT id FROM nbamatch.event WHERE match_id='{0}' and event_num='{1}' ", id, event_num)))
+                {
+                    MySqlHelper.Execute(string.Format("INSERT INTO nbamatch.event(match_id,period,time,game_time,type,team_id,person_id,description,gscore,hscore,event_num) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}')",
+                        id, period, updated, game_clock, type, team_id, personid, description, visitor_score, home_score, event_num));
+                }
+                else
+                {
+
+                }
+                
+            }
+        }
+
+        public static void getData(string id)
+        {
+            //id = "2017103005";
+            saveMatchInfo(id);
+            saveEventInfo(id);
+        }
+    }
+}
---
+++ NBAMatchDataGetter/NBAMatchDataGetter/WebHelper.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
+using System.Drawing;
+
+namespace NBAMatchDataGetter
+{
+    class WebHelper
+    {
+        /// <summary>
+        /// 通过post方式发送数据
+        /// </summary>
+        /// <param name="postString"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string postData(string postString, string url)
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "application/x-www-form-urlencoded");
+            //string postString = "";
+            byte[] postData = Encoding.Default.GetBytes(postString);
+            byte[] responseData = client.UploadData(url, "POST", postData);
+            string srcString = Encoding.UTF8.GetString(responseData);
+            return srcString;
+        }
+
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        /// <param name="Url">url</param>
+        /// <param name="postDataStr">GET数据</param>
+        /// <param name="cookie">GET容器</param>
+        /// <param name="ispost">是否是POST方式</param>
+        /// <returns></returns>
+        public static string getDataWithCookie(string url, string param, string cookieStr, Encoding charset, bool ispost)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //if (cookie.Count == 0)
+            //{
+            //    request.CookieContainer = new CookieContainer();
+            //    cookie = request.CookieContainer;
+            //}
+            //else
+            //{
+            //    request.CookieContainer = cookie;
+            //}
+            if (ispost) request.Method = "POST";
+            else request.Method = "GET";
+            request.ContentType = "text/html;charset=UTF-8";
+            request.Headers.Add("Cookie", cookieStr);
+
+            if (ispost == true && param.Length > 0)
+            {
+                request.ContentType = "application/x-www-form-urlencoded";
+                byte[] data = charset.GetBytes(param);
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+
+            //request.ContentType = "image/JPEG;";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+                StreamReader myStreamReader = new StreamReader(myResponseStream, charset);
+                string retString = myStreamReader.ReadToEnd();
+                myStreamReader.Close();
+                myResponseStream.Close();
+                return retString;
+            }
+            catch (Exception)
+            {
+                return "";
+
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// 通过GET image
+        /// </summary>
+        /// <param name="Url">url</param>
+        /// <param name="postDataStr">GET数据</param>
+        /// <param name="cookie">GET容器</param>
+        /// <returns></returns>
+        public static Image getJPGWithCookie(string url, string cookieStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            //if (cookie.Count == 0)
+            //{
+            //    request.CookieContainer = new CookieContainer();
+            //    cookie = request.CookieContainer;
+            //}
+            //else
+            //{
+            //    request.CookieContainer = cookie;
+            //}
+
+            request.Method = "GET";
+            request.Headers.Add("Cookie", cookieStr);
+            request.ContentType = "image/JPEG;";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream myResponseStream = response.GetResponseStream();
+
+                Image img = Image.FromStream(myResponseStream);
+
+
+                myResponseStream.Close();
+                return img;
+            }
+            catch (Exception)
+            {
+                return null;
+
+            }
+
+
+        }
+
+        /// <summary>
+        /// 检查ssh验证结果是否接受的函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="certificate"></param>
+        /// <param name="chain"></param>
+        /// <param name="errors"></param>
+        /// <returns></returns>
+        private static bool CheckValidationResult(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors errors)
+        {
+            return true; //总是接受     
+        }
+
+        private static readonly string DefaultUserAgent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)";
+
+        /// <summary>
+        /// 发送https请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="charset"></param>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        public static HttpWebResponse CreatePostHttpResponse(string url, Encoding charset, IDictionary<string, string> parameters = null)
+        {
+            HttpWebRequest request = null;
+            //HTTPSQ请求  
+            ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+            request = WebRequest.Create(url) as HttpWebRequest;
+            request.ProtocolVersion = HttpVersion.Version10;
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.UserAgent = DefaultUserAgent;
+            //如果需要POST数据     
+            if (!(parameters == null || parameters.Count == 0))
+            {
+                StringBuilder buffer = new StringBuilder();
+                int i = 0;
+                foreach (string key in parameters.Keys)
+                {
+                    if (i > 0)
+                    {
+                        buffer.AppendFormat("&{0}={1}", key, parameters[key]);
+                    }
+                    else
+                    {
+                        buffer.AppendFormat("{0}={1}", key, parameters[key]);
+                    }
+                    i++;
+                }
+                byte[] data = charset.GetBytes(buffer.ToString());
+                using (Stream stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            return request.GetResponse() as HttpWebResponse;
+        }
+
+        /// <summary>
+        /// 使用webclient下载文件
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="cookie"></param>
+        /// <param name="savepath"></param>
+        public static void saveFile(string url, string cookie, string savepath)
+        {
+            //string imgPath = getPath(textBox4.Text) + imgname + ".jpg";
+            WebClient client = new WebClient();
+            client.Headers.Add("Cookie", cookie);
+            try
+            {
+                byte[] bytes = client.DownloadData(url);
+                using (MemoryStream ms = new MemoryStream(bytes))
+                {
+                    Image image = System.Drawing.Image.FromStream(ms);
+                    image.Save(savepath);
+                    //this.pictureBox1.Image = image;
+                }
+            }
+            catch
+            {
+                return;
+            }
+
+        }
+
+        /// <summary>
+        /// 用httpWebRequest方式访问一个url，获取页面内容
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string getData(string url, Encoding charset)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "GET";
+            httpWebRequest.Timeout = 20000;
+            try
+            {
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream(), charset);
+                string responseContent = streamReader.ReadToEnd();
+
+                httpWebResponse.Close();
+                streamReader.Close();
+                return responseContent;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+
+        }
+    }
+}
